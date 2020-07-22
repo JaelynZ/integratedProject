a627d4955f4ea20e22f9897f526c167fc9bb3cab
jingling.zhang <jingling.zhang@ucarinc.com>
2020-07-22 14:57:24 +0800

    feat: 设计模式，模板方法模式

---
+++ src/main/java/com/jaelyn/integrated/module/design/TemplateMethodPattern.java
@@ -0,0 +1,52 @@
+package com.jaelyn.integrated.module.design;
+
+/**
+ * 模板方法模式
+ *
+ **/
+public class TemplateMethodPattern {
+    public static void main(String[] args) {
+        AbstractClass abc = new ConcreteClassA();
+        abc.templateMehthod();
+        abc = new ConcreteClassB();
+        abc.templateMehthod();
+    }
+
+    // 抽象模板类
+    static abstract class AbstractClass {
+        protected void step1() {
+            System.out.println("AbstractClass:step1");
+        }
+
+        protected void step2() {
+            System.out.println("AbstractClass:step2");
+        }
+
+        protected void step3() {
+            System.out.println("AbstractClass:step3");
+        }
+
+        // 声明为final方法，避免子类覆写
+        public final void templateMehthod() {
+            this.step1();
+            this.step2();
+            this.step3();
+        }
+    }
+
+    // 具体实现类A
+    static class ConcreteClassA extends AbstractClass {
+        @Override
+        protected void step1() {
+            System.out.println("ConcreateClassA:step1");
+        }
+    }
+
+    // 具体实现类B
+    static class ConcreteClassB extends AbstractClass {
+        @Override
+        protected void step2() {
+            System.out.println("ConcreateClassB:step2");
+        }
+    }
+}
